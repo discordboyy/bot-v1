ddb2a6823a38a71b220b57531b7a06141c2f0ee2
Makar <123008172+discordboyy@users.noreply.github.com>
2026-06-07 13:58:50 +0200

    Create game.py

---
+++ handlers/game.py
@@ -0,0 +1,13 @@
+import random
+
+async def game(update, context):
+    result = random.randint(1, 10)
+
+    if result <= 7:
+        await update.message.reply_text(
+            f"🏓 Ты отбил мяч! Выпало {result}"
+        )
+    else:
+        await update.message.reply_text(
+            f"❌ Промах! Выпало {result}"
+        )
