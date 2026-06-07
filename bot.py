a2d0418a38d8b39b8d15aac613e5e74aa3e580f0
Makar <123008172+discordboyy@users.noreply.github.com>
2026-06-07 21:54:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,14 @@ from telegram.ext import Application, CommandHandler
 from handlers.start import start
 from handlers.ping import ping
 from handlers.game import game
+from handlers.finance import finance_handler
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-
 def main():
+    if not TOKEN:
+        raise ValueError("BOT_TOKEN is missing")
+
     app = Application.builder().token(TOKEN).build()
 
     # handlers
@@ -16,11 +19,12 @@ def main():
     app.add_handler(CommandHandler("ping", ping))
     app.add_handler(CommandHandler("game", game))
 
+    # finance
+    app.add_handler(finance_handler)
+
     print("Bot is running...")
 
-    # ❗ правильный запуск (BLOCKING)
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
