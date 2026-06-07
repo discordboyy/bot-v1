f4bd0090d59805ca338da2b75a2e8434914b0306
Makar <123008172+discordboyy@users.noreply.github.com>
2026-06-07 14:26:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,27 @@
-from telegram.ext import Application
+import asyncio
+import os
+
+from telegram.ext import Application, CommandHandler
 
 from handlers.start import start
 from handlers.ping import ping
 from handlers.game import game
 
-from telegram.ext import CommandHandler
+TOKEN = os.environ["BOT_TOKEN"]
 
-import os
+async def main():
+    app = Application.builder().token(TOKEN).build()
 
-TOKEN = os.getenv("BOT_TOKEN")
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("ping", ping))
+    app.add_handler(CommandHandler("game", game))
 
-app = Application.builder().token(TOKEN).build()
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("ping", ping))
-app.add_handler(CommandHandler("game", game))
+    while True:
+        await asyncio.sleep(3600)
 
-app.run_polling()
+if __name__ == "__main__":
+    asyncio.run(main())
